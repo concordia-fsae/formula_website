cbecc0a51fd5ae18ef8ee59709e128690f2ebbc5
tonynokart <thomasson5leo@gmail.com>
2023-12-22 19:15:38 +0100

    js files for each html so that header + google maps work

--- team.js
+++ team.js
@@ -1,46 +1,3 @@
-//work in progress
-
-const menu = document.querySelector('.menu');
-const close = document.querySelector('.close');
-const nav = document.querySelector('nav');
-
-
-
-menu.addEventListener('click', () => {
-    nav.classList.add('open-nav');
-})
-close.addEventListener('click', () => {
-    nav.classList.remove('open-nav');
-})
-//these final three closes the menu when an item of the menu is clicked
-const aboutUs = document.getElementById('aboutUs');
-const subsystems = document.getElementById('subsystems');
-const contact = document.getElementById('contact')
-subsystems.addEventListener('click', () => {
-  nav.classList.remove('open-nav');
-})
-aboutUs.addEventListener('click', () => {
-  nav.classList.remove('open-nav');
-})
-contact.addEventListener('click', () => {
-  nav.classList.remove('open-nav');
-})
-
-
-//when arrow1 or arrow2 is clicked the screen scrolls down
-const arrow1 = document.querySelector('.arrow.one');
-const arrow2 = document.querySelector('.arrow.two');
-
-arrow1.addEventListener('click', () => {
-    window.scrollTo(0, 740) ;
-})
-arrow2.addEventListener('click', () => {
-    window.scrollTo(0, 1400) ;
-})
-
-
-
-
 //This makes header background appear white when scrolling down 20 px.
 window.onscroll = function() {scrollFunction()};
 
@@ -58,8 +15,6 @@ function scrollFunction() {
     document.querySelector("header").style.background = "unset";
   }
 }
-
-
 
 // this is the full google maps java code
 // Initialize and add the map
